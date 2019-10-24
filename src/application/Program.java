6e58dcf1f733bddeb3d1fa5d8426a324209247df
miltonferreira <jotonferreira@gmail.com>
2019-10-24 07:01:49 -0300

    implementado update na classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,13 @@ public class Program {
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 		*/
 		
+		// >>>>>>>>>>>>>>>>>>>> insere um novo vendedor ao sistema <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+		System.out.println("\n=== Test 5: seller insert ===");
+		seller = sellerDao.findById(1); // carrega os dados do ID selecionado
+		seller.setName("Martha Wayne"); // muda o nome do vendedor
+		sellerDao.update(seller); // passa o obj para atualizar no banco de dados
+		System.out.println("Update Completed");
+		
 		/*
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
--- src/model/dao/Impl/SellerDaoJDBC.java
+++ src/model/dao/Impl/SellerDaoJDBC.java
@@ -24,9 +24,9 @@ public class SellerDaoJDBC implements SellerDao{
 		this.conn = conn;
 	}
 	
+	// insere novo vendedor a tabela no BD -----------------------------
 	@Override
 	public void insert(Seller obj) {
-		//
 		
 		PreparedStatement st = null; // responsavel por inseri as informações no BD
 		
@@ -73,10 +73,38 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 
 	}
-
+	
+	// atualização de dados de um vendedor -----------------------------
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null; // responsavel por inseri as informações no BD
+		
+		try {
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?",
+					Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			// ao executar atualização de vendedor
+			st.executeUpdate();
+			
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
@@ -85,7 +113,8 @@ public class SellerDaoJDBC implements SellerDao{
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	// procura vendedor pelo ID
 	@Override
 	public Seller findById(Integer id) {
 		
@@ -153,10 +182,11 @@ public class SellerDaoJDBC implements SellerDao{
 		return dep;
 		
 	}
-
+	
+	// busca todos os vendedores em ordena por nome -----------------------------
 	@Override
 	public List<Seller> findAll() {
-		// busca todos os vendedores em ordena por nome
+		
 		
 		PreparedStatement st = null;
 		ResultSet rs = null;
@@ -207,10 +237,11 @@ public class SellerDaoJDBC implements SellerDao{
 			
 		}
 	}
-
+	
+	// busca vendedores pela id do departamento retornando uma lista de vendedores
 	@Override
 	public List<Seller> findByDepartment(Department department) {
-		// busca vendedores pela id do departamento retornando uma lista de vendedores
+		
 		
 		PreparedStatement st = null;
 		ResultSet rs = null;
